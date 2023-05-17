dacda6a030d811f802a038f3a1007c3b874bd963
Kenneth Rodriguez <78935468+KBlacksmith@users.noreply.github.com>
2023-05-17 14:25:02 -0600

    Actualizar data cleaning
    Limpiar del dataframe las ganancias en 0 y presupuestos en 0

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,6 +3,20 @@ import pandas as pd
 def clean_data(filename: str)->pd.DataFrame: 
     df = pd.read_csv(filename)
     df = df.dropna()
+    df = df.drop_duplicates()
+    for x in df.index: 
+        if df.loc[x, 'budget'] <= 0 or df.loc[x, 'revenue'] <= 0: 
+            df.drop(x, inplace=True)
+    return df
+
+def test_df()->pd.DataFrame: 
+    technologies = {
+    'Courses':["Spark","PySpark","Hadoop","Python","PySpark","Spark"],
+    'Fee' :[20000,25000,26000,22000,24000,35000],
+    'Duration':['30day','40days','35days','40days','60days','60days'],
+    'Discount':[1000,2300,1200,2500,2000,2000]
+              }
+    df = pd.DataFrame(technologies)
     return df
 
 if __name__=="__main__": 
